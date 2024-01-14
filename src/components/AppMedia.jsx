5197644bfe71a1b9a667a4fee1b26cae8c74797d
Xist0 <x1spridcherd@gmail.com>
2024-01-14 14:54:34 +0500

    1

--- src/components/AppMedia.jsx
+++ src/components/AppMedia.jsx
@@ -6,14 +6,14 @@ function AppMedia() {
     return (
         <div className="container">
             <div className="block-top">
-                <div className="block"><NavLink to="/Orders">Заказы</NavLink></div>
-                <div className="block"><NavLink to="/Employees">Сотрудники</NavLink></div>
-                <div className="block"><NavLink to="/Calls">Записи звонков</NavLink></div>
-                <div className="block"><NavLink to="/OrderStatus">Состояние заказа</NavLink></div>
-                <div className="block"><NavLink to="/SpareParts">Запчасти</NavLink></div>
-                <div className="block"><NavLink to="/Works">Работы</NavLink></div>
-                <div className="block"><NavLink to="/WarrantyRepair">Гарантийные ремонты</NavLink></div>
-                <div className="block"><NavLink to="/ChangeOrder">Изменить заказ</NavLink></div>
+                <NavLink to="/Orders" className="block"><div >Заказы</div></NavLink>
+                <NavLink to="/Employees" className="block"><div >Сотрудники</div></NavLink>
+                <NavLink to="/Calls" className="block"><div >Записи звонков</div></NavLink>
+                <NavLink to="/OrderStatus"className="block"><div >Состояние заказа</div></NavLink>
+                <NavLink to="/SpareParts"className="block"><div >Запчасти</div></NavLink>
+                <NavLink to="/Works" className="block"><div >Работы</div></NavLink>
+                <NavLink to="/WarrantyRepair" className="block"><div >Гарантийные ремонты</div></NavLink>
+                <NavLink to="/ChangeOrder" className="block"><div >Изменить заказ</div></NavLink>
             </div>
         </div>
     )
