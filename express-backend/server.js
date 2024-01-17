42e60affddc1d78d24a78caf1fb2b22d8e52df8f
Xist0 <x1spridcherd@gmail.com>
2024-01-17 22:27:08 +0500

    1

--- express-backend/server.js
+++ express-backend/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
-
+const fetch = require('node-fetch');
 
 const app = express();
 const port = 3000;
 
-// Разрешение CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -14,25 +13,33 @@ app.use((req, res, next) => {
     next();
 });
 
-// Сертификат безопасности
 const options = {
     key: fs.readFileSync('./CRMServe-private.key'),
     cert: fs.readFileSync('./CRMServe.crt')
 };
 
-// Запрос к внешнейБэку
-app.get('', async (req, res) => {
+app.use(async (req, res, next) => {
     try {
-        // Делаем GET-запрос к другому бэкенду на HTTP
-        const response = await axios.get('http://192.168.1.68');
+        // Убрал ожидание запроса с фронта
+        const response = await fetch('http://192.168.1.68');
 
-        // Возвращаем данные из ответа в вашем HTTPS бэкенде
-        res.json(response.data);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const responseData = await response.json();
+
+        res.json(responseData);
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
     }
 });
+
+app.get('/status', (req, res) => {
+    res.send('Server is running');
+});
+
 const server = https.createServer(options, app);
 
 server.listen(port, () => {
